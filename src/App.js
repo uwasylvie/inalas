5c54644b5601bf9c7ecf1f05ee01481b4fa0de2c
uwasylvie <u.sylvie@outlook.com>
2023-11-01 10:57:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import AboutUs from './components/AboutUs/AboutUs';
+
+import Work from "./components/Work/Work";
+
+import Services from "./components/Services/Services";
+
+import Contact from "./components/Contact/Contact";
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="holder">
+      <Header />
+      <AboutUs />
+      
+      <Work />
+      
+      <Services />
+      
+      <Contact />
+      <Footer />
     </div>
   );
 }
